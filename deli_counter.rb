bc2d5146b7c90602de8916e2d19e236e18984fc2
Terrence Bell <terrencekbell@gmail.com>
2020-03-05 04:32:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,4 +12,7 @@ def line(katz_deli)
 end  
 end
 
-def take_a_number()
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+  return "Welcome #{name}. You are number #{katz_deli} in line."
+end
