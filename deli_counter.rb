62a323638c5b7aeeb2644752be00c5657e2ef710
Terrence Bell <terrencekbell@gmail.com>
2020-03-05 04:14:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   array = []
  if katz_deli.length >= 1
     katz_deli.each do |name|
-    array.push("#{array}. #{name}.")
+    array.push("#{counter}. #{name}.")
     counter += 1
     end
     puts "The line is currently:#{array}"
